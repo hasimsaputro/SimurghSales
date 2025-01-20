0eb62a914c3d86734634c9d46b0172a8d01db4fb
hasimsaputro <179096240+hasimsaputro@users.noreply.github.com>
2025-01-20 14:33:26 +0700

    dataleadsform anjaiii

---
+++ src/main/java/com/sales/controller/DataLeadsController.java
@@ -0,0 +1,18 @@
+package com.sales.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("DataLeads")
+public class DataLeadsController {
+    @GetMapping("")
+    public String index(Model model){
+        return "sales/data-leads";
+    }
+
+    @GetMapping("insert")
+    public String insert(Model model) { return "sales/data-leads-form";}
+}
