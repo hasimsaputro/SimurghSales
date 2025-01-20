92703643aed30ca92e3136b89de1f646b1d1db8a
winangsit12 <134899986+winangsit12@users.noreply.github.com>
2025-01-20 15:23:41 +0700

    Add entity Bank

---
+++ src/main/java/com/sales/entity/Bank.java
@@ -0,0 +1,20 @@
+package com.sales.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "Bank")
+public class Bank {
+    @Id
+    @Column(name = "Id", nullable = false)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "NamaBank", length = 100, nullable = false)
+    private String namaBank;
+
+    @Column(name = "Status", nullable = false)
+    private Boolean status;
+}
