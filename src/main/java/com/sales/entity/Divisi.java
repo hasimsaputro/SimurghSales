4097a85f2a4fc50ec713e9632fc8786932166c55
winangsit12 <134899986+winangsit12@users.noreply.github.com>
2025-01-20 14:58:07 +0700

    Entity Divisi

---
+++ src/main/java/com/sales/entity/Divisi.java
@@ -0,0 +1,24 @@
+package com.sales.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+@Entity
+@Table(name = "Divisi")
+public class Divisi {
+    @Id
+    @Column(name = "Id", length = 5, nullable = false)
+    private String id;
+
+    @Column(name = "NamaDivisi", length = 100, nullable = false)
+    private String namaDivisi;
+
+    @Column(name = "Status", nullable = false)
+    private Boolean status;
+
+    @OneToMany(mappedBy = "divisi")
+    private List<Departement> departements;
+}
