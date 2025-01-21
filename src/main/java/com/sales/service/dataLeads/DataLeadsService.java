0c7b4232ab68a9c94043d35c367f412a87251e13
winangsit12 <134899986+winangsit12@users.noreply.github.com>
2025-01-21 13:52:55 +0700

    getMitraAgenById

--- src/main/java/com/sales/service/dataLeads/DataLeadsService.java
+++
@@ -1,9 +0,0 @@
-package com.sales.service.dataLeads;
-
-import com.sales.dto.dataLeads.DataLeadsIndexDTO;
-
-import java.util.List;
-
-public interface DataLeadsService {
-    List<DataLeadsIndexDTO> getAll(String fullName, String search, int page);
-}
