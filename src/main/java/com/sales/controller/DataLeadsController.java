9367a3a346571f9d3396d19905dc6d44e2960a1d
GXYRU <96215279+XYGALAB@users.noreply.github.com>
2025-01-21 11:31:17 +0700

    commit entity

--- src/main/java/com/sales/controller/DataLeadsController.java
+++ src/main/java/com/sales/controller/DataLeadsController.java
@@ -1,15 +1,33 @@
 package com.sales.controller;
 
+import com.sales.service.dataLeads.DataLeadsService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
-@RequestMapping("DataLeads")
+@RequestMapping("dataleads")
 public class DataLeadsController {
+    private final DataLeadsService service;
+
+    @Autowired
+    public DataLeadsController(DataLeadsService service) {
+        this.service = service;
+    }
+
     @GetMapping("")
-    public String index(Model model){
+    public String index(@RequestParam(defaultValue = "") String filter,
+                        @RequestParam(defaultValue = "") String search,
+                        @RequestParam(defaultValue = "1") int page, Model model){
+        var grid = service.getAll(filter,search,page);
+//        int total = service.getTotal(fullName);
+        model.addAttribute("grid", grid);
+//        model.addAttribute("totalPages",total);
+        model.addAttribute("currentPage",page);
+//        model.addAttribute("fullName",fullName);
         return "sales/data-leads";
     }
 
