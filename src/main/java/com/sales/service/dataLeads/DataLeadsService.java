2a8cda2d296959de58687795583b3f13937fb69a
AsyariMaufuri <179084121+AsyariMaufuri@users.noreply.github.com>
2025-01-21 13:11:06 +0700

    Service Dataleaads

--- src/main/java/com/sales/service/dataLeads/DataLeadsService.java
+++ src/main/java/com/sales/service/dataLeads/DataLeadsService.java
@@ -1,9 +1,12 @@
 package com.sales.service.dataLeads;
 
+import com.sales.dto.dataLeads.DataLeadsFormDTO;
 import com.sales.dto.dataLeads.DataLeadsIndexDTO;
 
 import java.util.List;
 
 public interface DataLeadsService {
     List<DataLeadsIndexDTO> getAll(String fullName, String search, int page);
+
+    void updateInsertDataLeads(DataLeadsFormDTO dataLeadsFormDTO);
 }
